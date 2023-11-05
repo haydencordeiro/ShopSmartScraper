d953140135869b35e398f510814d43c0d3e60344
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-11-05 12:55:13 -0500

    made changes to send data to send data to mongodb

--- src/main/java/selenium/LaunchChrome.java
+++ src/main/java/selenium/LaunchChrome.java
@@ -1,21 +1,33 @@
 package selenium;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
+import com.google.gson.Gson;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.Keys;
+
 import java.util.ArrayList; // import the ArrayList class
 import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class LaunchChrome {
+    public static class Product{
+        String productName;
+        String productSellingPrice;
+        String productThumbnail;
+        String productComparisonDetails;
 
+    }
     public static ArrayList<String> getWebsites(){
     	ArrayList<String> output = new ArrayList<String>();
-    	String[] searchTerms = { "Eggs", "Apples" };
+    	String[] searchTerms = { "Eggs" };
         String[] mainURLS = {"https://www.zehrs.ca/search?search-bar=","https://www.nofrills.ca/search?search-bar="};
         for(String mainURL: mainURLS) {            	
         	for(String searchTerm: searchTerms) {
@@ -24,53 +36,79 @@ public class LaunchChrome {
         }
         return output;
     }
+
+    public static String convertDataToJson(ArrayList<Product> dataBase ){
+        Gson gson = new Gson();
+        return gson.toJson(dataBase);
+    }
+
+    public static void sendDataToDatabase(String data){
+
+    }
+
+    public static void sendPostRequest(String endpoint, String jsonData) throws Exception {
+        URL url = new URL(endpoint);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            try (OutputStream outputStream = connection.getOutputStream()) {
+                byte[] input = jsonData.getBytes(StandardCharsets.UTF_8);
+                outputStream.write(input, 0, input.length);
+            }
+
+            int responseCode = connection.getResponseCode();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            }
+
+        } finally {
+            connection.disconnect();
+        }
+    }
     
     @SuppressWarnings("deprecation")
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
     	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        
 
-//        driver.get("https://www.zehrs.ca/");
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//        WebDriverWait.until(By.id("search-form-input"));
 
-        
-//        WebElement searchBox = driver.findElement(By.cssSelector(".search-input__input"));
-//        searchBox.sendKeys("apples");
-//
-//        
-//        searchBox.sendKeys(Keys.RETURN);
-//        Below code lists all the products in the zehrs
-
-        
         ArrayList<String> websites = getWebsites();
-        
-        HashMap<String, HashMap<String,String>> dataBase = new HashMap<String, HashMap<String,String>>();      
-        
+
+//        HashMap<String, HashMap<String,String>> dataBase = new HashMap<String, HashMap<String,String>>();
+        ArrayList<Product> dataBase = new ArrayList<Product>();
         for(int j=0;j<websites.size();j++)
         {
-        driver.get(websites.get(j));    
-//        System.out.println("Website  - "+websites.get(j));
+        driver.get(websites.get(j));
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         List<WebElement> productsDiv = driver.findElements(By.cssSelector("[class=\"product-tile\"]"));
-        String productName, sellingPrice, comparisionDetails;
-//        System.out.println("Product Name                       |Product Price                 |Description");
-        
+        String productName, sellingPrice, comparisionDetails,productThumbnail;
         for( int i =0 ; i < 3; i++) {
-        	productName = productsDiv.get(i).findElement(By.className("product-name--product-tile")).getText();
-        	sellingPrice = productsDiv.get(i).findElement(By.className("selling-price-list--product-tile")).getText();
-        	comparisionDetails = productsDiv.get(i).findElement(By.className("comparison-price-list__item__price")).getText();
-        	HashMap<String, String> productData = new HashMap<String,String>();
-        	productData.put("sellingPrice", sellingPrice);
-        	productData.put("comparisionDetails", comparisionDetails);
-        	dataBase.put(productName, productData);
-        	
-//        	System.out.println(productName+"            "+sellingPrice+"                  "+comparisionDetails);
+            Product newProduct = new Product();
+            newProduct.productThumbnail = productsDiv.get(i).findElement(By.className("responsive-image--product-tile-image")).getAttribute("src");
+            newProduct.productName = productsDiv.get(i).findElement(By.className("product-name--product-tile")).getText();
+            newProduct.productSellingPrice = productsDiv.get(i).findElement(By.className("selling-price-list--product-tile")).getText();
+            newProduct.productComparisonDetails = productsDiv.get(i).findElement(By.className("comparison-price-list__item__price")).getText();
+
+            dataBase.add(newProduct);
         }
-//        System.out.println("-----------------------------------------------------------------------------------------------");
         }
         System.out.println(dataBase);
-        driver.close();    }
+        driver.close();
+
+        String jsonString = convertDataToJson(dataBase);
+        System.out.println(jsonString);
+
+        sendPostRequest("http://localhost:8080/insertdata", jsonString);
+    }
     }
 
